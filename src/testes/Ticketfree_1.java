b659333b6f0c3dc666c22408f4ce71aaf8cb47e2
Krefit <gustavokrawczyk@gmail.com>
2021-06-11 08:55:38 -0300

    Inicialização do projeto
    Criação da priemira classe (versão de teste)

---
+++ src/testes/Ticketfree_1.java
@@ -0,0 +1,24 @@
+package testes;
+
+public class Ticketfree_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//DECLARAÇÃO DE VARIAVEIS de definição do tamanho de cada sessão do teatro
+		int qtdPlateiaA,qtdPlateiaB,qtdFrisa,qtdCamarote,qtdBalcaoNobre;
+		qtdPlateiaA = 25;
+		qtdPlateiaB = 100;
+		qtdFrisa = 5;
+		qtdCamarote = 10;
+		qtdBalcaoNobre = 50;
+		
+		iniciaPrograma(qtdPlateiaA,qtdPlateiaB,qtdFrisa,qtdCamarote,qtdBalcaoNobre);
+	}
+	
+	private static void iniciaPrograma(int qtdPlateiaA, int qtdPlateiaB, int qtdFrisa, int qtdCamarote, int qtdBalcaoNobre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
