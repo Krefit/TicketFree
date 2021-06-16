9b159cb3ae884be78b8d0f46b995221fb56b5534
Krefit <gustavokrawczyk@gmail.com>
2021-06-15 22:04:50 -0300

    Seleção de assento
    Criado metodo para inicializar a matriz com os "valores" dos assentos, imprimir a matriz e selecionar assentos nelas.
    
    Foi feito alteração nos metodos de compra para que fosse "reutilizavel" as matrizes

--- src/testes/Ticketfree_1.java
+++ src/testes/Ticketfree_1.java
@@ -41,20 +41,26 @@ public class Ticketfree_1 {
 	public static void iniciaPrograma(int qtdPlateiaA, int qtdPlateiaB, int qtdFrisa, int qtdCamarote, int qtdBalcaoNobre) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int matrizPlateiaB[][] = new int[10][10];
-		int matrizPlateiaA[][] = new int[5][5];
-		int matrizPlateiaFrisa[][] = new int[6][5];
-		int matrizPlateiaCamarote[][] = new int[5][10];
-		int matrizPlateiaBalcNobre[][] = new int[5][10];
 		
 		int menu;
+		String matrizPlateiaB[][] = new String[10][10]; //10*10 = 100
+		String matrizPlateiaA[][] = new String[5][5];// 5*5 = 25
+		String matrizPlateiaFrisa[][] = new String[6][5]; //existem 6 plateias Frisa - cada plateia = uma letra
+		String matrizPlateiaCamarote[][] = new String[5][10]; //existem 5 camarotes - cada camarote = uma letra
+		String matrizPlateiaBalcNobre[][] = new String[5][10]; //5*10 = 50
+		
+		inicializaAssentos(matrizPlateiaA,"A");
+		inicializaAssentos(matrizPlateiaB,"B");
+		inicializaAssentos(matrizPlateiaCamarote, "CA");
+		inicializaAssentos(matrizPlateiaFrisa, "F");
+		inicializaAssentos(matrizPlateiaBalcNobre, "BN");
 
 		do { // Executando o DO WHILE para executar pelo menos uma vez o programa
 			chamaMenu();
 			menu = sc.nextInt();
 
 			if (menu == 1){
-				compraIngresso(); //Se escolheu fazer compra, chama função de comprar ingresso
+				compraIngresso(matrizPlateiaB,matrizPlateiaA,matrizPlateiaFrisa,matrizPlateiaCamarote,matrizPlateiaBalcNobre); //Se escolheu fazer compra, chama função de comprar ingresso
 			}else if (menu == 2) {
 				estatisticaVendas(); // se escolheu mostrar estatisticas, chama função de estatisticas de venda
 			}
@@ -66,8 +72,10 @@ public class Ticketfree_1 {
 		System.out.println("Retorna Estatisticas");
 	}
 
-	public static void compraIngresso() {
+	public static void compraIngresso(String matrizPlateiaB[][],String matrizPlateiaA[][],String matrizPlateiaFrisa[][],String matrizPlateiaCamarote[][],String matrizPlateiaBalcNobre[][]) {
 		// TODO Auto-generated method stub
+		//criação das matrizes
+		
 		Scanner sc = new Scanner(System.in);
 		int sessao, area, menu;
 		float preco=0;
@@ -105,20 +113,27 @@ public class Ticketfree_1 {
 		 * */
 				
 		switch (area) {
-			case 1: 
+			case 1: //Plateia A
 				preco = 40;
+				
+
+				matrizPlateiaA = escolherAssento(matrizPlateiaA);
 				break;
-			case 2:
+			case 2: //Plateia B
 				preco = 60;
+				matrizPlateiaB = escolherAssento(matrizPlateiaB);
 				break;
-			case 3: 
+			case 3: //Frisa
 				preco = 120;
+				matrizPlateiaFrisa = escolherAssento(matrizPlateiaFrisa);
 				break;
-			case 4:
+			case 4: //Camarote
 				preco = 80;
+				matrizPlateiaCamarote = escolherAssento(matrizPlateiaCamarote);
 				break;
-			case 5:
+			case 5: //Balcão Nobre
 				preco = 250;
+				matrizPlateiaBalcNobre = escolherAssento(matrizPlateiaBalcNobre);
 				break;
 			default:
 				System.out.println("Area não existente");
@@ -133,11 +148,54 @@ public class Ticketfree_1 {
 		
 		if(menu == 1)
 		{
-			compraIngresso();
+			compraIngresso(matrizPlateiaB,matrizPlateiaA,matrizPlateiaFrisa,matrizPlateiaCamarote,matrizPlateiaBalcNobre);
 		}//se não, sai da função.
 	}
 	
-	public static void imprimeIngresso(String CPF, int Sessao, int Area, float Preco)
+	public static void inicializaAssentos(String matriz[][], String Nome) {
+		int contador=1;
+		for (int i = 0; i < matriz.length; i++) {//letras
+			for (int j = 0; j < matriz[0].length; j++) {//numero
+				matriz[i][j] = Nome+contador;
+				contador++;
+			}
+		}
+	}
+	
+	public static void imprimeMatriz(String matriz[][])
+	{
+		for (int i = 0; i < matriz.length; i++) {//letras
+			for (int j = 0; j < matriz[0].length; j++) {//numero
+				System.out.print("["+matriz[i][j]+"]");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static  String[][] escolherAssento(String matriz[][]) {
+		Scanner sc = new Scanner(System.in);
+		String assento;
+		//Loop para "Demonstrar cadeiras.
+		System.out.println("Escolher assento do cliente:");
+		imprimeMatriz(matriz);
+		assento = sc.next();		
+		for (int i = 0; i < matriz.length; i++) {//letras
+			for (int j = 0; j < matriz[0].length; j++) {//numero
+				if(matriz[i][j].compareTo(assento) == 0)
+				{
+					matriz[i][j] = "X";
+				}
+			}
+		}
+		System.out.println();
+		System.out.println();
+		
+		imprimeMatriz(matriz);
+		return matriz;
+		
+	}
+	
+	public static void imprimeIngresso(String CPF, int Sessao, int Area, float Preco /*, posicao*/)
 	{
 		System.out.println("**********************************************");
 		System.out.println("**                 Ingresso                 **");
@@ -145,6 +203,7 @@ public class Ticketfree_1 {
 		System.out.println("** Sessao: "+Sessao+"          **");
 		System.out.println("** Area: "+Area+"              **");
 		System.out.println("** Preco: R$"+Preco+"          **");
+		//System.out.println("** Assento: "+posicao+"          **");
 		System.out.println("**********************************************");
 	}
 
